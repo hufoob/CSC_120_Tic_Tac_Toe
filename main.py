85c2b02d3d058789a77b76ca82c4275991eefa53
hufoob <chrisesmith96@gmail.com>
2021-12-08 21:45:31 -0500

    Added logic for winning, losing, and draw.

--- main.py
+++ main.py
@@ -22,9 +22,21 @@ def main():
         row = int(input("What row would you like to choose?: "))
         col = int(input("What column would you like to choose?: "))
         legal_move = check_mark(row - 1, col - 1, init_board)
-        if legal_move == 1:
+        if legal_move:
             place_mark(row - 1, col - 1, player_id, init_board)
-            player_id = change_player(player_id)
+            winner = check_win(init_board)
+            tie = check_tie(init_board)
+            if winner:
+                board.print_board(init_board)
+                print(f"Player {player_id} wins!")
+                break
+            elif tie:
+                board.print_board(init_board)
+                print("It's a tie! No more moves.")
+                break
+            else:
+                player_id = change_player(player_id)
+                continue
         else:
             print("You can't do that, try again.")
             continue
@@ -64,6 +76,7 @@ def check_mark(row, col, init_board):
         else:
             return False
 
+
 def place_mark(row, col, player_id, init_board):
     # -----------------------------------------------------------------
     # Changes the value in the list of lists to represent player input
@@ -74,7 +87,47 @@ def place_mark(row, col, player_id, init_board):
         init_board[row][col] = 'O'
 
 
-# def check_win (row,col,player_id):
+def check_win(init_board):
+    # -----------------------------------------------------------------
+    # I had trouble finding the most "pythonic" way to solve this, but
+    # this function checks each way a user can win, by rows it does
+    # this through iterating over the rows and checking if all elements
+    # are the same. For vertical and diagonal, I ended up brute forcing
+    # it to check if the exact 3 spots you can win diagonally and
+    # vertically.
+    # -----------------------------------------------------------------
+    # Check row for winner by taking using set, which only takes unique elements
+    # If all the elements in the row are the same, the length of the set will only be 1
+
+    for row in init_board:
+        if '-' not in row:
+            if len(set(row)) == 1:
+                return True
+    # Checks for a winner via the columns by checking each column on the board using
+    # a for loop, and ignores values that are "empty" i.e. equal to '-'
+    for i in range(len(init_board)):
+        if init_board[i][0] == init_board[i][1] == init_board[i][2] != "-" \
+                or init_board[0][i] == init_board[1][i] == init_board[2][i] != "-":
+            return True
+    # Checks the exact points on the board that would need to be the same to ensure
+    # a win diagonally, and ignores values that are "empty" i.e. equal to '-'
+    if init_board[0][0] == init_board[1][1] == init_board[2][2] != '-' \
+            or init_board[0][2] == init_board[1][1] == init_board[2][0] != '-':
+        return True
+
+
+def check_tie(init_board):
+    # -----------------------------------------------------------------
+    # Uses a nested for loop to check if there is a still an "empty"
+    # spot on the board. If there are no free spots, returns True
+    # which tells the main function to stop the game and call it a tie.
+    # -----------------------------------------------------------------
+
+    for row in init_board:
+        for i in row:
+            if i == '-':
+                return False
+    return True
 
 
 main()
